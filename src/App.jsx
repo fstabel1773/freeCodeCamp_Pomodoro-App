a4f17e3b83f3ff6b31de7b5baff4e64b176b65db
Florian Stabel <f.stabel@gmx.de>
2023-02-24 08:33:25 +0100

    feat: timeFormatter for Countdown

--- src/App.jsx
+++ src/App.jsx
@@ -10,104 +10,109 @@ function App() {
   const [isRunning, setIsRunning] = useState(false)
 
   function handleIncrement(event) {
-    if (event.target.id === "break-increment") {
-      if (breakLength < 60) {
-        setBreakLength(prevBreakLength => prevBreakLength + 1)
-      }
-    } else if (sessionLength < 60) {
-        setSessionLength(prevSessionLength => prevSessionLength + 1)
-      }
+    if (event.target.id === "break-increment"
+      || event.target.parentElement.id === "break-increment") {
+        if (breakLength < 60) {
+          setBreakLength(prevBreakLength => prevBreakLength + 1)
+        }} else if (sessionLength < 60) {
+          setSessionLength(prevSessionLength => prevSessionLength + 1)
+        }
   }
 
   function handleDecrement(event) {
-    if (event.target.id === "break-decrement") {
-      if (breakLength > 0) {
-        setBreakLength(prevBreakLength => prevBreakLength - 1)
-      }
-    } else if (sessionLength > 0) {
-        setSessionLength(prevSessionLength => prevSessionLength - 1)
-      }
+    if (event.target.id === "break-decrement"
+      || event.target.parentElement.id === "break-decrement") {
+        if (breakLength > 0) {
+          setBreakLength(prevBreakLength => prevBreakLength - 1)
+        }} else if (sessionLength > 0) {
+          setSessionLength(prevSessionLength => prevSessionLength - 1)
+        }
   }
 
-  
+  function timeFormatter(seconds) {
+    const min = Math.floor(seconds / 60)
+    const sec = seconds % 60
+    const minStr = min < 10 ? `0${min}` : `${min}`
+    const secStr = sec < 10 ? `0${sec}` : `${sec}`
+    const timeFormatted = `${minStr}:${secStr}`
+    return timeFormatted
+  }
+
 
   return (
     <div className="vh-100 d-flex justify-content-center align-items-center bg-warning">
-      <div id="App-Wrapper" className="container p-5 border border-5 border-dark rounded w-50">
+      <div id="App-Wrapper" className="p-5 border border-5 border-dark rounded w-50">
 
-        <div className="row">
-          <h1 className="col text-center">Pomodoro-Clock</h1>
-        </div>
+        <h1 className="col text-center">Pomodoro-Clock</h1>
 
-        <div className="row my-3">
-          <div className="col-6">
-            <div className="row">
-              <h3 id="break-label" 
-                className="text-center"
-                >Break Length</h3>
-            </div>
-            <div className='row'>
-              <h3 className="text-center">
-                <i id="break-decrement" 
-                  className="bi bi-arrow-down-square"
-                  onClick={handleDecrement}
-                  ></i>
-                <span id="break-length" className="mx-2">
-                  {breakLength}
-                </span>
-                <i id="break-increment" 
-                  className="bi bi-arrow-up-square pe-auto"
-                  onClick={handleIncrement}
-                  ></i>
-              </h3>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="row">
-              <h3 id="session-label" className="text-center">Session Length</h3>
-            </div>
-            <div className='row'>
-              <h3 className="text-center">
-                <i id="session-decrement" 
-                  className="bi bi-arrow-down-square"
-                  onClick={handleDecrement}
-                  ></i>
-                <span id="session-length" className="mx-2">
-                  {sessionLength}
-                </span>
-                <i id="session-increment" 
-                  className="bi bi-arrow-up-square pe-auto"
-                  onClick={handleIncrement}
-                  ></i>
-              </h3>
-            </div>
-          </div>
-        </div>
-
-        <div className="row w-50 mx-auto my-3 py-4 border border-4 border-dark rounded-3">
-          <div className="col-12 text-center">
-            <h4 id="timer-label" className="">Session</h4>
-          </div>
-          <div className="col-12 text-center">
-            <h1 id="time-left">
-              25:00
-            </h1>
-          </div>
-        </div>
-
-        <div className="row w-50 mx-auto my-3">
-          <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-            <div className="btn-group btn-group-lg mr-2 mx-auto" role="group" aria-label="First group">
-              <button id="start_stop" type="button" className="btn btn-dark text-warning">
-                <i className="bi bi-play-fill"></i>
-                <i className="bi bi-pause-fill"></i>
+        <div className="break-session-length">
+          <div className="">
+            <h3 id="break-label" 
+              className="text-center">
+              Break Length
+            </h3>
+            <div className='d-flex align-items-center justify-content-center'>
+              <button 
+                id="break-decrement" 
+                onClick={handleDecrement}
+                className="btn btn-lg btn-dark text-warning">
+                <i className="bi bi-arrow-down-square"></i>
               </button>
-              <button id="reset" type="button" className="btn btn-dark text-warning">
-                <i className="bi bi-arrow-clockwise"></i>
+              <h3 id="break-length" className="mx-2">
+                {breakLength}
+              </h3>
+              <button 
+                id="break-increment"
+                onClick={handleIncrement}
+                className="btn btn-lg btn-dark text-warning">
+                <i className="bi bi-arrow-up-square"></i>
+              </button>
+            </div>
+          </div>
+          <div className="">
+            <h3 id="session-label" 
+              className="text-center">
+              Session Length
+            </h3>
+            <div className='d-flex align-items-center justify-content-center'>
+              <button 
+                id="session-decrement" 
+                onClick={handleDecrement}
+                className="btn btn-lg btn-dark text-warning">
+                <i className="bi bi-arrow-down-square"></i>
+              </button>
+              <h3 id="session-length" className="mx-2">
+                {sessionLength}
+              </h3>
+              <button 
+                id="session-increment"
+                onClick={handleIncrement}
+                className="btn btn-lg btn-dark text-warning">
+                <i className="bi bi-arrow-up-square"></i>
               </button>
             </div>
           </div>
         </div>
+
+        <div className="w-50 mx-auto my-3 py-4 border border-4 border-dark rounded-3">
+          <h4 id="timer-label" className="text-center">Session</h4>
+          <h1 id="time-left" className="text-center">
+            {timeFormatter(timeLeft)}
+          </h1>
+        </div>
+ 
+        <div className="btn-toolbar w-50 mx-auto my-3" role="toolbar" aria-label="Toolbar with button groups">
+          <div className="btn-group btn-group-lg mr-2 mx-auto" role="group" aria-label="First group">
+            <button id="start_stop" type="button" className="btn btn-dark text-warning">
+              <i className="bi bi-play-fill"></i>
+              <i className="bi bi-pause-fill"></i>
+            </button>
+            <button id="reset" type="button" className="btn btn-dark text-warning border-start">
+              <i className="bi bi-arrow-clockwise"></i>
+            </button>
+          </div>
+        </div>
+
 
       </div>
     </div>
